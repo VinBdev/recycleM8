fbd1f87fcdc4ca2f61f6897b8509960316bddbd0
VinBdev <vbpod77@gmail.com>
2022-01-24 19:37:38 +0000

    Fixed js bug on emails

--- assets/js/email.js
+++ assets/js/email.js
@@ -7,22 +7,31 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            $('button-a').click(function(){
+                swal({
+                  title:"confirm",
+                  text:"Are you sure?",
+                  buttons: {
+                    cancel:true,
+                    confirm: "Submit"
+                  }
+                });
+      });
         
         },
         function(error) {
             console.log("FAILED", error);
         }
-    );
+       ); $('button-a').click(function(){
+            swal({
+              title:"Error",
+              text:"Please check the information submitted",
+              buttons: {
+                cancel:true, 
+              }
+            });
+          });
+    
     return false;   
 }
 
-$('button-a').click(function(){
-    swal({
-      title:"confirm",
-      text:"Are you sure?",
-      buttons: {
-        cancel:true,
-        confirm: "Submit"
-      }
-    })
-  });
