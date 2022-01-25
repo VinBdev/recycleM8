128bf0d57e8b8edbdf62ce93b0946347c7151daf
VinBdev <vbpod77@gmail.com>
2022-01-25 15:29:04 +0000

    Fixed email js error in sending email + compressed hero image

--- assets/js/email.js
+++ assets/js/email.js
@@ -20,3 +20,14 @@ function sendMail(contactForm) {
     return false;   
 };
 
+function sweetAlertClick(){
+    Swal.fire({
+        title: 'Custom animation with Animate.css',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+      });
+}
