bf32f13d5f8c4ad86328d151d275b0bd19f7a87b
VinBdev <vbpod77@gmail.com>
2022-01-26 00:13:29 +0000

    Fixing bug on contact page with email sending

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,37 +1,17 @@
 function sendMail(contactForm) {
     emailjs.send("service_fuvx6ut", "RecycleM8", {
         "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
+        "from_email": contactForm.emailaddress.value, 
         "message": contactForm.message.value
     })
     .then(
         function(response) {
             console.log("SUCCESS", response);
-            $('button-a').click(function(){
-                swal({
-                  title:"confirm",
-                  text:"Are you sure?",
-                  buttons: {
-                    cancel:true,
-                    confirm: "Submit"
-                  }
-                });
-      });
-
+        
         },
         function(error) {
             console.log("FAILED", error);
         }
     );
-       ); $('button-a').click(function(){
-            swal({
-              title:"Error",
-              text:"Please check the information submitted",
-              buttons: {
-                cancel:true, 
-              }
-            });
-          });
-
     return false;   
 }
